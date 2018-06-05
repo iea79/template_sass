d1ee3f85b09a6834080e02c643650fa4a497950c
Evgeny Ivanov <busforward@gmail.com>
2018-06-06 00:03:26 +0500

    Доработка шаблона
    1. Дописал загтовку под формы
    2. Разделил стили
    3. Добавил скрипты в function.js

--- js/function.js
+++ js/function.js
@@ -78,7 +78,10 @@ $(document).ready(function() {
     //             }
     //     });
     // });
-   	// setGridMatch($('[data-grid-match] .grid__item'));
+
+    // Inputmask.js
+    // $('[name=tel]').inputmask("+9(999)999 99 99",{ showMaskOnHover: false });
+
    	gridMatch();
 });
 
@@ -92,7 +95,6 @@ $(window).resize(function(event) {
 });
 
 function checkOnResize() {
-   	// setGridMatch($('[data-grid-match] .grid__item'));
    	gridMatch();
 }
 
@@ -102,17 +104,219 @@ function gridMatch() {
    	});
 }
 
-// function setGridMatch(columns) {
-// 	var tallestcolumn = 0;
-// 	columns.removeAttr('style');
-// 	columns.each( function() {
-// 		currentHeight = $(this).height();
-// 		if(currentHeight > tallestcolumn) {
-// 			tallestcolumn = currentHeight;
-// 		}
-// 	});
-// 	setTimeout(function() {
-// 		columns.css('minHeight', tallestcolumn);
-// 	}, 100);
+function fontResize() {
+    var windowWidth = $(window).width();
+    if (windowWidth < 1440 && windowWidth >= 768) {
+    	var fontSize = windowWidth/19.05;
+    } else if (windowWidth < 768) {
+    	var fontSize = 50;
+    // } else if (windowWidth >= 1770) {
+    // 	var fontSize = 100;
+    }
+	$('body').css('fontSize', fontSize + '%');
+}
+
+// Видео youtube для страницы
+$(function () {
+    if ($(".js-youtube")) {
+        $(".js-youtube").each(function () {
+            // Зная идентификатор видео на YouTube, легко можно найти его миниатюру
+            $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/sddefault.jpg)');
+
+            // Добавляем иконку Play поверх миниатюры, чтобы было похоже на видеоплеер
+            $(this).append($('<img src="img/play.svg" alt="Play" class="video__play">'));
+
+        });
+
+        $('.video__play, .video__prev').on('click', function () {
+            // создаем iframe со включенной опцией autoplay
+            var videoId = $(this).closest('.youtube').attr('id');
+            var iframe_url = "https://www.youtube.com/embed/" + videoId + "?autoplay=1&autohide=1";
+            if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
+
+            // Высота и ширина iframe должны быть такими же, как и у родительского блока
+            var iframe = $('<iframe/>', {
+                'frameborder': '0',
+                'src': iframe_url,
+                'width': $(this).width(),
+                'height': $(this).innerHeight()
+            })
+
+            // Заменяем миниатюру HTML5 плеером с YouTube
+            $(this).closest('.video__wrapper').append(iframe);
+
+        });
+    }
+
+});
+
+
+function formSubmit() {
+    $("[type=submit]").on('click', function (e){ 
+        e.preventDefault();
+        var form = $(this).closest('.form');
+        var url = form.attr('action');
+        var form_data = form.serialize();
+        var field = form.find('[required]');
+        // console.log(form_data);
+
+        empty = 0;
+
+        field.each(function() {
+            if ($(this).val() == "") {
+                $(this).addClass('invalid');
+                // return false;
+                empty++;
+            } else {
+                $(this).removeClass('invalid');
+                $(this).addClass('valid');
+            }  
+        });
+
+        // console.log(empty);
+
+        if (empty > 0) {
+            return false;
+        } else {        
+            $.ajax({
+                url: url,
+                type: "POST",
+                dataType: "html",
+                data: form_data,
+                success: function (response) {
+                    // $('#success').modal('show');
+                    // console.log('success');
+                    // console.log(response);
+                    // console.log(data);
+                    document.location.href = "success.html";
+                },
+                error: function (response) {
+                    // $('#success').modal('show');
+                    // console.log('error');
+                    // console.log(response);
+                }
+            });
+        }
+
+    });
+
+    $('[name="policyConfirm"]').on('change', function(event) {
+        event.preventDefault();
+        var btn = $(this).closest('.form').find('.btn');
+        if ($(this).prop('checked')) {
+            btn.removeAttr('disabled');
+            // console.log('checked');
+        } else {
+            btn.attr('disabled', true);
+        }
+    });
+}
+
+// Деление чисел на разряды Например из строки 10000 получаем 10 000
+// Использование: thousandSeparator(1000) или используем переменную.
+// function thousandSeparator(str) {
+//     var parts = (str + '').split('.'),
+//         main = parts[0],
+//         len = main.length,
+//         output = '',
+//         i = len - 1;
+    
+//     while(i >= 0) {
+//         output = main.charAt(i) + output;
+//         if ((len - i) % 3 === 0 && i > 0) {
+//             output = ' ' + output;
+//         }
+//         --i;
+//     }
+
+//     if (parts.length > 1) {
+//         output += '.' + parts[1];
+//     }
+//     return output;
+// };
+
+
+// Хак для яндекс карт втавленных через iframe
+// Страуктура:
+//<div class="map__wrap" id="map-wrap">
+//  <iframe style="pointer-events: none;" src="https://yandex.ru/map-widget/v1/-/CBqXzGXSOB" width="1083" height="707" frameborder="0" allowfullscreen="true"></iframe>
+//</div>
+// Обязательное свойство в style которое и переключет скрипт
+// document.addEventListener('click', function(e) {
+//     var map = document.querySelector('#map-wrap iframe')
+//     if(e.target.id === 'map-wrap') {
+//         map.style.pointerEvents = 'all'
+//     } else {
+//         map.style.pointerEvents = 'none'
+//     }
+// })
+
+// Простая проверка форм на заполненность и отправка аяксом
+// function formSubmit() {
+//     $("[type=submit]").on('click', function (e){ 
+//         e.preventDefault();
+//         var form = $(this).closest('.form');
+//         var url = form.attr('action');
+//         var form_data = form.serialize();
+//         var field = form.find('[required]');
+//         // console.log(form_data);
+
+//         empty = 0;
+
+//         field.each(function() {
+//             if ($(this).val() == "") {
+//                 $(this).addClass('invalid');
+//                 // return false;
+//                 empty++;
+//             } else {
+//                 $(this).removeClass('invalid');
+//                 $(this).addClass('valid');
+//             }  
+//         });
+
+//         // console.log(empty);
+
+//         if (empty > 0) {
+//             return false;
+//         } else {        
+//             $.ajax({
+//                 url: url,
+//                 type: "POST",
+//                 dataType: "html",
+//                 data: form_data,
+//                 success: function (response) {
+//                     // $('#success').modal('show');
+//                     // console.log('success');
+//                     console.log(response);
+//                     // console.log(data);
+//                     // document.location.href = "success.html";
+//                 },
+//                 error: function (response) {
+//                     // $('#success').modal('show');
+//                     // console.log('error');
+//                     console.log(response);
+//                 }
+//             });
+//         }
+
+//     });
+
+//     $('[required]').on('blur', function() {
+//         if ($(this).val() != '') {
+//             $(this).removeClass('invalid');
+//         }
+//     });
+
+//     $('.form__privacy input').on('change', function(event) {
+//         event.preventDefault();
+//         var btn = $(this).closest('.form').find('.btn');
+//         if ($(this).prop('checked')) {
+//             btn.removeAttr('disabled');
+//             // console.log('checked');
+//         } else {
+//             btn.attr('disabled', true);
+//         }
+//     });
 // }
+
 
